adaa94a20b86de4ac07efa62b5943d679d4c9d8c
Lee Ren Jie <work.renjie@gmail.com>
2021-03-29 23:07:04 +0800

    Responsive and redesigned about page

--- frontend/src/about/components/MyInfo.js
+++ frontend/src/about/components/MyInfo.js
@@ -5,8 +5,8 @@ import styles from './MyInfo.module.css'
 const MyInfo = () => {
     return(
         <div>
-            <div className={styles.title}>
-                <h1>About me</h1>
+            <div className={styles["title-container"]}>
+                <h1 className={styles.title}>About Me</h1>
             </div>
             <MyInfoCard />
         </div>
